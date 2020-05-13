b9101bf4dfed65f8602a35a3e4336dfc0d0c8452
SathishSaminathan <Sathishcse1975@gmail.com>
2020-05-13 23:59:24 +0530

    commit

--- src/screens/Home/LocationResults.js
+++ src/screens/Home/LocationResults.js
@@ -1,40 +1,40 @@
-import * as React from "react";
+import * as React from 'react';
 import {
   View,
   StyleSheet,
   Dimensions,
   TouchableOpacity,
   Platform,
-} from "react-native";
-import { TabView, SceneMap, TabBar } from "react-native-tab-view";
-import Modal from "react-native-modal";
-import FAB from "react-native-fab";
-import { Colors } from "../../constants/ThemeConstants";
-import TextComponent from "../../components/Shared/TextComponent";
-import IconComponent from "../../components/Shared/IconComponent";
-import ButtonComponent from "../../components/Shared/ButtonComponent";
-import Upcoming from "../Stays/Upcoming";
-import Past from "../Stays/Past";
-import Cancelled from "../Stays/Cancelled";
-import SearchLocation from "./SearchLocation";
-import PickDate from "./PickDate";
-import { IconType, FontType } from "../../constants/AppConstants";
-import LocationResultList from "./LocationResultList";
-import NearByEvents from "./NearByEvents";
-import HeaderComponent from "../../components/Shared/HeaderComponent";
-import RoomPreferences from "./RoomPreferences";
-import Ripple from "react-native-material-ripple";
-import IOSLocationResults from "./IOSLocationResults";
-import { StatusBarHeight } from "../../helpers/styleHelper";
+} from 'react-native';
+import {TabView, SceneMap, TabBar} from 'react-native-tab-view';
+import Modal from 'react-native-modal';
+import FAB from 'react-native-fab';
+import {Colors} from '../../constants/ThemeConstants';
+import TextComponent from '../../components/Shared/TextComponent';
+import IconComponent from '../../components/Shared/IconComponent';
+import ButtonComponent from '../../components/Shared/ButtonComponent';
+import Upcoming from '../Stays/Upcoming';
+import Past from '../Stays/Past';
+import Cancelled from '../Stays/Cancelled';
+import SearchLocation from './SearchLocation';
+import PickDate from './PickDate';
+import {IconType, FontType} from '../../constants/AppConstants';
+import LocationResultList from './LocationResultList';
+import NearByEvents from './NearByEvents';
+import HeaderComponent from '../../components/Shared/HeaderComponent';
+import RoomPreferences from './RoomPreferences';
+import Ripple from 'react-native-material-ripple';
+import IOSLocationResults from './IOSLocationResults';
+import {StatusBarHeight} from '../../helpers/styleHelper';
 
-const initialLayout = { width: Dimensions.get("window").width };
+const initialLayout = {width: Dimensions.get('window').width};
 
 const LocationResults = (props) => {
   const [index, setIndex] = React.useState(0);
   const [ModalVisible, setModalVisible] = React.useState(false);
   const [routes] = React.useState([
-    { key: "LocationResultList", title: "Nashville Rooms" },
-    { key: "NearByEvents", title: "Nearby Events" },
+    {key: 'LocationResultList', title: 'Nashville Rooms'},
+    {key: 'NearByEvents', title: 'Nearby Events'},
   ]);
 
   const renderScene = SceneMap({
@@ -48,19 +48,18 @@ const LocationResults = (props) => {
     setIndex(newIdx);
   };
 
-  const labels = ({ title }) => {
+  const labels = ({title}) => {
     switch (title) {
-      case "Nearby Events":
+      case 'Nearby Events':
         return (
           <View>
             <TextComponent
               style={{
-                fontFamily: "ProximaNova-Bold",
+                fontFamily: 'ProximaNova-Bold',
                 fontSize: 15,
                 color: Colors.tabText,
                 paddingVertical: 10,
-              }}
-            >
+              }}>
               {title}
             </TextComponent>
           </View>
@@ -68,25 +67,23 @@ const LocationResults = (props) => {
 
       default:
         return (
-          <View style={{ flex: 1 }}>
+          <View style={{flex: 1}}>
             <TextComponent
               style={{
-                fontFamily: "ProximaNova-Bold",
+                fontFamily: 'ProximaNova-Bold',
                 fontSize: 15,
                 color: Colors.tabText,
                 paddingVertical: 0,
-              }}
-            >
+              }}>
               {title}
             </TextComponent>
             <TextComponent
               style={{
-                fontFamily: "ProximaNova-Regular",
+                fontFamily: 'ProximaNova-Regular',
                 fontSize: 13,
                 color: Colors.tabText,
                 paddingVertical: 5,
-              }}
-            >
+              }}>
               {props.route.params.date}
             </TextComponent>
           </View>
@@ -94,14 +91,14 @@ const LocationResults = (props) => {
     }
   };
 
-  const renderLabel = ({ route }) => labels(route);
+  const renderLabel = ({route}) => labels(route);
 
   const renderHeader = (props) => (
     <TabBar
       // activeColor={Colors.primaryDarkThemeColor}
       // inactiveColor={Colors.grey}
-      indicatorStyle={{ backgroundColor: Colors.themeBlack, height: 1.5 }}
-      style={{ backgroundColor: Colors.white }}
+      indicatorStyle={{backgroundColor: Colors.themeBlack, height: 1.5}}
+      style={{backgroundColor: Colors.white}}
       renderLabel={renderLabel}
       {...props}
     />
@@ -111,17 +108,16 @@ const LocationResults = (props) => {
     <View
       style={{
         flex: 1,
-        paddingTop: Platform.OS === "ios" ? StatusBarHeight : 0,
+        paddingTop: Platform.OS === 'ios' ? StatusBarHeight : 0,
         backgroundColor: Colors.white,
-      }}
-    >
-      {Platform.OS === "ios" ? (
+      }}>
+      {Platform.OS === 'ios' ? (
         <IOSLocationResults {...props} />
       ) : (
-        <View style={{ flex: 1 }}>
+        <View style={{flex: 1}}>
           <HeaderComponent title="Search Results" hasBack {...props} />
           <TabView
-            navigationState={{ index, routes }}
+            navigationState={{index, routes}}
             renderTabBar={renderHeader}
             renderScene={renderScene}
             onIndexChange={setIndex}
@@ -149,8 +145,7 @@ const LocationResults = (props) => {
           backgroundColor: Colors.white,
           padding: 0,
           margin: 0,
-        }}
-      >
+        }}>
         {/* <Reeo */}
         <View
           style={{
@@ -158,46 +153,42 @@ const LocationResults = (props) => {
             backgroundColor: Colors.white,
             // borderBottomWidth: 1,
             borderBottomColor: Colors.accordionBorderColor,
-            flexDirection: "row",
-            alignItems: "center",
+            flexDirection: 'row',
+            alignItems: 'center',
             paddingHorizontal: 10,
             elevation: 5,
+            paddingTop: Platform.OS === 'ios' ? StatusBarHeight : 0,
             justifyContent:
-              Platform.OS === "ios" ? "space-between" : "flex-start",
-          }}
-        >
+              Platform.OS === 'ios' ? 'space-between' : 'flex-start',
+          }}>
           <Ripple
             rippleColor={Colors.white}
-            onPress={() => setModalVisible(!ModalVisible)}
-          >
+            onPress={() => setModalVisible(!ModalVisible)}>
             <View
               style={{
                 width: 40,
-                height: "100%",
+                height: '100%',
                 // alignItems: "center",
-                justifyContent: "center",
+                justifyContent: 'center',
                 borderRadius: 50,
-              }}
-            >
+              }}>
               <IconComponent
                 type={IconType.AntDesign}
                 name="left"
-                style={{ fontSize: 18 }}
+                style={{fontSize: 18}}
               />
             </View>
           </Ripple>
-          <TextComponent style={{ fontSize: 18 }} type={FontType.BOLD}>
+          <TextComponent style={{fontSize: 18}} type={FontType.BOLD}>
             Room & Rates Filters
           </TextComponent>
-          {Platform.OS === "ios" && (
+          {Platform.OS === 'ios' && (
             <TouchableOpacity
               activeOpacity={0.8}
-              onPress={() => setModalVisible(false)}
-            >
+              onPress={() => setModalVisible(false)}>
               <TextComponent
                 type={FontType.BOLD}
-                style={{ fontSize: 17, color: Colors.cancelblue }}
-              >
+                style={{fontSize: 17, color: Colors.cancelblue}}>
                 Done
               </TextComponent>
             </TouchableOpacity>
