e09ee2ab114bdf085c03d9eac9a3b382f184ab64
SathishSaminathan <Sathishcse1975@gmail.com>
2020-05-18 12:23:35 +0530

    commit for in app browser

--- src/screens/auth/Login.js
+++ src/screens/auth/Login.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
-import {Text, View, Image, StatusBar} from 'react-native';
+import {Text, View, Image, StatusBar, Alert, Linking} from 'react-native';
 import {authorize} from 'react-native-app-auth';
+import InAppBrowser from 'react-native-inappbrowser-reborn';
 
 import {heightPerc, widthPerc} from '../../helpers/styleHelper';
 import {Colors} from '../../constants/ThemeConstants';
@@ -28,6 +29,47 @@ const config = {
 };
 
 export default class Signup extends Component {
+  async openLink() {
+    try {
+      const url = 'https://www.google.com';
+      if (await InAppBrowser.isAvailable()) {
+        const result = await InAppBrowser.open(url, {
+          // iOS Properties
+          dismissButtonStyle: 'cancel',
+          preferredBarTintColor: '#453AA4',
+          preferredControlTintColor: 'white',
+          readerMode: false,
+          animated: true,
+          modalPresentationStyle: 'overFullScreen',
+          modalTransitionStyle: 'partialCurl',
+          modalEnabled: true,
+          enableBarCollapsing: false,
+          // Android Properties
+          showTitle: true,
+          toolbarColor: Colors.themeBlack,
+          secondaryToolbarColor: 'black',
+          enableUrlBarHiding: true,
+          enableDefaultShare: true,
+          forceCloseOnRedirection: false,
+          // Specify full animation resource identifier(package:anim/name)
+          // or only resource name(in case of animation bundled with app).
+          animations: {
+            startEnter: 'slide_in_right',
+            startExit: 'slide_out_left',
+            endEnter: 'slide_in_left',
+            endExit: 'slide_out_right',
+          },
+          headers: {
+            'my-custom-header': 'my custom header value',
+          },
+        });
+        // Alert.alert(JSON.stringify(result));
+      } else Linking.openURL(url);
+    } catch (error) {
+      Alert.alert(error.message);
+    }
+  }
+
   handleAmazon = () => {
     let temp = authorize(config);
     console.log(temp);
@@ -96,6 +138,7 @@ export default class Signup extends Component {
                 }}>
                 <ButtonComponent
                   // onPress={() => this.props.navigation.navigate('Home')}
+                  onPress={() => this.openLink()}
                   style={{backgroundColor: Colors.darkGrey, fontSize: 12}}
                   borderRadius={50}>
                   Log in
